e664e9ecb5b969150d1558d80429e504ed7132af
Oliulla <razasm29@gmail.com>
2023-12-20 20:46:46 +0600

    make an api for daily quote

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ async function dbAdmin() {
       .db("portfolio")
       .collection("projectDetails");
     const projectsCollection = client.db("portfolio").collection("projects");
+    const dailyQuotes = client.db("portfolio").collection("quotes");
+    console.log("db connected")
 
     app.get("/projects", async (req, res) => {
       try {
@@ -54,6 +56,36 @@ async function dbAdmin() {
       const details = filter[0].imgs;
       // console.log(details);
       res.send([title, details]);
+    });
+
+
+    // Send daily quote
+    app.get('/daily-quote', async (req, res) => {
+      try {
+        function getDayInfoFromDate() {
+          // Get the current date
+          const currentDate = new Date();
+
+          // Get the day of the year (1-based)
+          const startOfYear = new Date(currentDate.getFullYear(), 0, 1);
+          const dayNumber =
+            Math.ceil((currentDate - startOfYear) / (24 * 60 * 60 * 1000)) + 1;
+
+
+          return {
+            day: dayNumber,
+          };
+        }
+        const day = getDayInfoFromDate()
+        const dailyQuote = await dailyQuotes.findOne(day);
+        if (dailyQuote) {
+          res.status(200).json({ status: true, message: 'daily quote got successfully', data: dailyQuote });
+        } else {
+          res.json({ status: false, message: 'data got failed', data: [] });
+        }
+      } catch (error) {
+        res.json({ status: false, message: error.message });
+      }
     });
   } finally {
   }
